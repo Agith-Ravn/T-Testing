12999dd2dfb5d396ca31cb5a97126f3893c561d7
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-10-05 15:23:07 +0200

    .

---
+++ Practice12/Practice12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Practice12
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await TestClass.Test1();
+        }
+
+    }
+}
---
+++ Practice12/Practice12/TestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Practice12
+{
+    public class TestClass
+    {
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+
+        public static async Task Test1()
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/ditto");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
