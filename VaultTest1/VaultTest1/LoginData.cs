bf743ae105046df00767dfb89761ceab3bb3c58e
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-10-26 10:21:56 +0200

    .

---
+++ VaultTest1/VaultTest1/LoginData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultTest1
+{
+    class LoginData
+    {
+        public string vaultAdress { get; set; }
+
+        public string vaultAdress { get; set; }
+    }
+}
---
+++ VaultTest1/VaultTest1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VaultTest1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
